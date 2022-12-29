33a4f88114739716cec0357162b7622dce73fbc7
henrio randriasoa <rioxdev@gmail.com>
2022-12-29 20:15:08 +0100

    clean up

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -2,45 +2,39 @@
 using FluentValidation;
 using MediatR;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Application.Activities
+namespace Application.Activities;
+
+public class Create
 {
-    public class Create
+    public class Command : IRequest
     {
-        public class Command : IRequest
-        {
-            public Activity Activity { get; set; }
-        }
-
-        public class Handler : IRequestHandler<Command>
-        {
-            private readonly DataContext _context;
-            public Handler(DataContext context)
-            {
-                _context = context;
-            }
-
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
-            {
-                _context.Activities.Add(request.Activity);
-
-                await _context.SaveChangesAsync();
-                return Unit.Value;
-            }
-        }
-
-        public class CommandValidator : AbstractValidator<Command>
-        {
-            public CommandValidator()
-            {
-                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
-            }
-        }
-
+        public Activity Activity { get; set; }
     }
+
+    public class Handler : IRequestHandler<Command>
+    {
+        private readonly DataContext _context;
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        {
+            _context.Activities.Add(request.Activity);
+
+            await _context.SaveChangesAsync();
+            return Unit.Value;
+        }
+    }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+        }
+    }
+
 }
--- Application/Activities/Update.cs
+++ Application/Activities/Update.cs
@@ -3,11 +3,6 @@ using Domain;
 using FluentValidation;
 using MediatR;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Activities;
 
